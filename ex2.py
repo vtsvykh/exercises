7d665744c3c9ce948e1e9a977e16cc84afb8d297
vtsvykh <v.tsvykh0@gmail.com>
2023-10-06 12:34:52 +0700

    add programm

--- ex2.py
+++ ex2.py
@@ -1,6 +1,7 @@
 file = open('input.txt', 'r')
 out_file = open('output.txt', 'w')
 for line in file:
-    if line[0] == 'A':
+    if line[0] == 'A' or line[0] == 'a':
+        out_file.write(line)
 
 
