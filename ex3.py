4554adad0911c72722d24fe83e396000a0b5bda8
vtsvykh <v.tsvykh0@gmail.com>
2023-10-06 13:03:04 +0700

    add programm

---
+++ ex3.py
@@ -0,0 +1,11 @@
+inp_file = open('input.txt', 'r')
+out_file = open('output.txt', 'w')
+s = ''
+for line in inp_file:
+    s += line
+
+string = s.split()
+
+for line in string:
+    if line[-1]:
+        out_file.write(line[-1])
