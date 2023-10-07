356e1ef5be4cca50082d042ce4af991649a9cbbb
vtsvykh <v.tsvykh0@gmail.com>
2023-10-07 20:04:28 +0700

    add exercises 4

--- ex4.py
+++ ex4.py
@@ -1,19 +1,23 @@
 name_file = input('Введите имя файла (Например: myfamily.txt): ')
-num_line_file = int(input('Введите номер строки из выбранного файла (целое число): '))
-file = open(name_file, 'r')
-s = ''
+num_line_file = input('Введите номер строки из выбранного файла (целое число): ')
+
 result = ''
 
-for line in file:
-    s += line
+try:
+    file = open(name_file, 'r')
+    strings = file.readlines()
 
-strings = s.split()
+    for index in range(len(strings)):
+        if index == int(num_line_file) - 1:
+            result = strings[index]
+            break
+        else:
+            result = 'Строки под таким номером нет! Попробуйте еще раз.'
 
-for index in range(len(strings)):
-    if index == num_line_file - 1:
-        result = strings[index]
-        break
-    else:
-        result = 'Строки под таким номером нет! Попробуйте еще раз.'
+except FileNotFoundError:
+    print('Файл не найден. Попробуйте еще раз.')
+
+except ValueError:
+    print('Некорректно задан номер строки. Попробуйте еще раз.')
 
 print(result)
